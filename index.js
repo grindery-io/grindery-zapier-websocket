9adbf63f0650eebbe8a8652f49d159a9e00b9b80
Connex Digital <clientaccess@connex.digital>
2022-09-01 01:08:10 -0400

    changes

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { fromString } = require("uuidv4");
+const { v5 } = require("uuid");
 const port = 3000;
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 const uri =
   "mongodb+srv://connex_testing:MkLvwusz9i2K7mOT@cluster0.5d0qb9x.mongodb.net/?retryWrites=true&w=majority";
@@ -15,21 +23,47 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-/*app.listen(process.env.PORT || port, () => {
-  console.log(`Example app listening on port ${port}`);
-});*/
+app.listen(process.env.PORT || port, () => {
+  console.log(`Listening on port ${port}`);
+});
 
-app.post("/webhooks", (req, res) => {
-  client.connect((err) => {
-    const collection = client.db("test").collection("webhooks");
+app.post("/webhooks", async (req, res) => {
+  client.connect(async (err) => {
+    const collection = client.db("grindery_zapier").collection("webhooks");
     // perform actions on the collection object
-
+    //console.log(req.body);
+    const hook_url = req.body.url;
+    const hook_token = fromString(hook_url);
+    const new_webhook = {
+      timestamp: Date.now(),
+      token: hook_token,
+      webhook_url: hook_url,
+    };
+    const insert_result = await collection.insertOne(new_webhook);
+    console.log(
+      `A document was inserted with the _id: ${insert_result.insertedId}`
+    );
     client.close();
+    //res.status(200).send({ data: "ok" });
+    res
+      .status(200)
+      .json({ zap_token: hook_token, id: insert_result.insertedId });
   });
 });
 
-app.delete("/webhooks/:webhook_id", (req, res) => {
-  //find by id
+app.delete("/webhooks/:webhook_id", async (req, res) => {
+  const { webhook_id } = req.params;
+
+  client.connect(async (err) => {
+    client.db("grindery_zapier").collection("webbooks");
+    const collection = client.db("grindery_zapier").collection("webhooks");
+    const insert_result = await collection.deleteOne({
+      _id: new ObjectId(webhook_id),
+    });
+    console.log(`A document was deleted with the _id: ${webhook_id}`);
+    client.close();
+    res.status(200).json({ result: "removed" });
+  });
 });
 
 module.exports = app;
