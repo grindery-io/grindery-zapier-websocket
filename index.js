e84670c957f8b3bd89f191fc5ae3b8ca5a419333
Connex Digital <clientaccess@connex.digital>
2022-09-30 12:46:47 -0400

    response

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ app.ws("/", function (ws, req) {
         //test if response is success
         //demo test
         const response_success = { jsonrpc: "2.0", result: true, id: 1 };
-        ws.send(JSON.stringify(response_success));
+        ws.send('{"jsonrpc": \'2.0\',"result":true, "id":1}');
         //return response_success;
         //console.log("Response from Zapier: ", forward_to_zap);
       }
