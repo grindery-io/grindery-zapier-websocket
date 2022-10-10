f48c8bca376841561f120f1c95bc71267013ed7d
Connex Digital <clientaccess@connex.digital>
2022-10-10 10:34:32 -0400

    updates

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const port = 3000;
 const axios = require("axios");
 var expressWs = require("express-ws")(app);
 
-const uri =
-  "mongodb+srv://connex_testing:MkLvwusz9i2K7mOT@cluster0.5d0qb9x.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${processenv.mongo_user}:${process.env.mongo_password}@cluster0.5d0qb9x.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
