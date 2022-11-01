7f45cf34bb85fdb5c861995cbb4ef488d241a7e3
Connex Digital <clientaccess@connex.digital>
2022-11-01 07:39:39 -0400

    update

--- index.js
+++ index.js
@@ -156,14 +156,14 @@ wss.on("connection", (ws) => {
         }
         if (dataJSON.method === "setupSignal") {
           console.log("setupSignal Method from Client ", ws.id);
-          /*const new_signal_token = {
+          const new_signal_token = {
             $set: {
-              [dataJSON.params.fields.token]: {
+              [dataJSON.params.fields.token]: dataJSON.params.fields.token,
                 ws_id: ws.id,
                 sessionId: dataJSON.params.sessionId,
               },
             },
-          };*/
+          };
           /*const new_signal_token = {
             $set: {
               token: dataJSON.params.fields.token,
@@ -175,12 +175,7 @@ wss.on("connection", (ws) => {
           //associate connection with token
           const insert_signal_result = await collection.updateOne(
             { [dataJSON.params.fields.token]: dataJSON.params.fields.token },
-            {
-              $set: {
-                ws_id: ws.id,
-                sessionId: dataJSON.params.sessionId,
-              },
-            },
+            new_signal_token,
             { upsert: true }
           );
         }
