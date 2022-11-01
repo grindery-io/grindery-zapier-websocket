2558989854ca7485f46375e321d96a2602c302b9
Connex Digital <clientaccess@connex.digital>
2022-11-01 11:41:08 -0400

    stringify object

--- index.js
+++ index.js
@@ -208,7 +208,10 @@ wss.on("connection", (ws) => {
 
           if (search_result_token) {
             console.log("Found Zap URL", search_result_token.webhook_url);
-            console.log("Data from Action: ", dataJSON.params.fields.data);
+            console.log(
+              "Data from Action: ",
+              JSON.stringify(dataJSON.params.fields.data)
+            );
             const forward_to_zap = await axios.post(
               search_result_token.webhook_url,
               {
