d7d5f8c14ff6bfb3bf0bb8a590daeaa1b19de088
Connex Digital <clientaccess@connex.digital>
2022-11-01 15:32:49 -0400

    added payload to latest data

--- index.js
+++ index.js
@@ -186,6 +186,7 @@ wss.on("connection", (ws) => {
               $set: {
                 token: dataJSON.params.fields.token,
                 timestamp: Date.now(),
+                data: JSON.stringify(dataJSON.params.fields.data),
               },
             };
             const insert_test = await data_transmissions.updateOne(
